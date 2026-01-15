579d1cb03212785ee891c003e1d1aca603c43449
Ando Wikandono <ahmadwikandono@MacBook-Air.local>
2026-01-15 18:30:19 +0700

    update changes syntax #3

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,8 +3,12 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   fullyParallel: false,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  // retries: process.env.CI ? 2 : 0,
+  // workers: process.env.CI ? 1 : undefined,
+  // timeout: 30000,
+  // reporter: [['list'], ['html', { outputFolder: 'playwright-report' }]],
+  // fullyParallel: true,
+  workers: 1,
   timeout: 30000,
   reporter: [['list'], ['html', { outputFolder: 'playwright-report' }]],
   use: {
