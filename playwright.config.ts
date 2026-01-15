a92ef5ddddc3b0a11440237a0547aa442dae2b7f
Ando Wikandono <ahmadwikandono@MacBook-Air.local>
2026-01-15 17:53:09 +0700

    update syntax #2

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,32 +3,26 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   fullyParallel: false,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : 4,
-  timeout: 30_000,
-
-  reporter: [
-    ['list'],
-    ['html', { outputFolder: 'playwright-report', open: 'never' }],
-  ],
-
+  // retries: process.env.CI ? 2 : 0,
+  // workers: process.env.CI ? 1 : undefined,
+  timeout: 30000,
+  reporter: [['list'], ['html', { outputFolder: 'playwright-report' }]], // { open: 'never' }]]
   use: {
-    headless: !!process.env.CI,
-    viewport: process.env.CI ? { width: 1920, height: 1080 } : null,
+    headless: false,
+    viewport: null,
     launchOptions: {
-      args: process.env.CI ? [] : ['--start-maximized'],
+      args: ['--start-maximized'],
     },
+
     ignoreHTTPSErrors: true,
     video: 'retain-on-failure',
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
   },
-
-  projects: process.env.CI
-    ? [{ name: 'chromium', use: { browserName: 'chromium' } }]
-    : [
-        { name: 'chromium', use: { browserName: 'chromium' } },
-        { name: 'firefox', use: { browserName: 'firefox' } },
-        { name: 'webkit', use: { browserName: 'webkit' } },
-      ],
+  
+  projects: [
+    { name: 'chromium', use: { browserName: 'chromium' } },
+    { name: 'firefox', use: { browserName: 'firefox' } },
+    { name: 'webkit', use: { browserName: 'webkit' } },
+  ],
 });
