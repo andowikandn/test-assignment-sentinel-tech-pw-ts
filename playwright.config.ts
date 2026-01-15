bcdead5a8be55cc0491eed0836d137aa2f46ab3f
Ando Wikandono <ahmadwikandono@MacBook-Air.local>
2026-01-15 17:28:37 +0700

    add github action playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,9 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   fullyParallel: false,
-  workers: 1,
+  // workers: 1,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
   timeout: 30000,
   reporter: [['list'], ['html', { outputFolder: 'playwright-report' }]], // { open: 'never' }]]
   use: {
